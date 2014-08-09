44f00ea614c46276b504b94478660669835f8bfe
Oliver Eggert <mail@olivereggert.de>
2014-08-09 15:31:56 +0200

    added the template file

---
+++ template.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+#
+# ProjectEuler.net - problem #XXX
+#
+# Summary: 
+
+import sys
+
+def main():
+    print "Implement me!"
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
